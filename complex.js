f738cd7dea30aca1736e0ab14433477244aeeedd
PFPF <PFPF@users.noreply.github.com>
2021-02-25 21:52:12 -0500

    Update complex.js
    Listed some constants

--- complex.js
+++ complex.js
@@ -1,10 +1,26 @@
 // Complex numbers are represented in the form [r, phi/pi], where r >= 0 and the second entry is in (-1, 1].
-// 0 = [0,0], -0 = [0,1], ComplexZero = [0,NaN], i = [1,0.5], -i = [1,-0.5], 0i = [0,0.5]
+// For example, 0 = [0,0], -0 = [0,1], ComplexZero = [0,NaN], i = [1,0.5], -i = [1,-0.5], 0i = [0,0.5]
 // Infinity = [Infinity,0], -Infinity = [Infinity,1], ComplexInfinity = [Infinity, NaN]
 
-// to check the results, do
-// vals = {"0":[0,0],"1":[1,0],"2":[2,0],"-0":[0,1],"-1":[1,1],"-2":[2,1],"0i":[0,0.5],"i":[1,0.5],"c0":[0,NaN],"inf":[Infinity,0],"-inf":[Infinity,1],"cinf":[Infinity,NaN],"nan":[NaN,NaN]};
-// Object.keys(vals).forEach(a => Object.keys(vals).forEach(b => console.log(a + " ^ " + b + " = (" + pow(vals[a], vals[b]) + ")")));
+const vals = {
+  "1": [1,0],
+  "2": [2,0],
+  "-1": [1,1], // which denotes 1*e^(1pi*i). [1,-1], [-1,0] are NOT allowed
+  "-2": [2,1],
+  "0": [0,0], // this is the limit 0^+, as in js
+  "-0": [0,1], // this is the limit 0^-, as in js
+  "i": [1,0.5], // the thing
+  "1+i": [Math.sqrt(2), 0.25],
+  "0i": [0,0.5], // this is the limit 0^+ * i. Can be obtained by sqrt(-0)
+  "C0": [0,NaN], // this is the generic complex zero (i.e. we know the modulus but not the argument)
+  "Inf": [Infinity,0], // reciprocal of +0
+  "-Inf": [Infinity,1], // reciprocal of -0
+  "Inf*i": [Infinity, 0.5], // reciprocal of 0i. Can be obtained by sqrt(-Inf). Similarly, there can be Inf*(1+i) := sqrt(sqrt(-Inf)) etc
+  "CInf": [Infinity,NaN], // the generic complex infinity, the reciprocal of C0
+  "NaN": [NaN,NaN], // one can argue that 1^Inf should be [NaN,0], but I thought if I allow [NaN,0] I'd also allow [NaN, anything]. So I'll use the same value.
+};
+// Totality pow check
+Object.keys(vals).forEach(a => Object.keys(vals).forEach(b => console.log(a + " ^ " + b + " = (" + pow(vals[a], vals[b]) + ")")));
 
 function assert(truth, desc) { if(truth) console.log(desc); else throw ("Assertion failed: " + desc); }
 function aae(arr1, arr2, desc) { assert(arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i] || isNaN(v) && isNaN(arr2[i])), desc); } // asserts arrays equal
